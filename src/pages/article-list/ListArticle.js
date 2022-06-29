59585bf0d40fd25703f00f450f0fe1644108eb28
FadilaPrasetyo8 <fadilaprasetyo8@gmail.com>
2022-06-29 20:58:51 +0700

    Update API Article

--- src/pages/article-list/ListArticle.js
+++ src/pages/article-list/ListArticle.js
@@ -1,20 +1,17 @@
-import React from 'react'
-import Content from '../../component/article/Content'
+import React from "react";
+import Content from "../../component/article/Content";
 
-const ListArticle = ({article}) => {
-    return(
-        <>
-            <header className="App-header">Article</header>
-            <div className='list-article'>
-                {
-                    article.map((artic) => (
-                        <Content {...artic} key={artic.id} />
-                    ))
-                }
-            </div>
-        </>
-       
-    )
-}
+const ListArticle = ({ article }) => {
+  return (
+    <>
+      <header className="App-header">Article</header>
+      <div className="list-article">
+        {article.map((articles) => (
+          <Content data={articles.attributes} key={articles.id} />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default ListArticle
+export default ListArticle;
