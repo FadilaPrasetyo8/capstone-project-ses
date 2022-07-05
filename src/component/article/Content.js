340c6689baeeacd918b0e4e9bee3e913fbd9b7af
FadilaPrasetyo8 <fadilaprasetyo8@gmail.com>
2022-07-05 21:33:55 +0700

    Update change footer, content, style

--- src/component/article/Content.js
+++ src/component/article/Content.js
@@ -4,22 +4,26 @@ import { ImageItem } from "./ImageItem";
 import { ContentItem } from "./ContentItem";
 import { ContentTitle } from "./ContentTitle";
 import { Link } from "react-router-dom";
-
 import { Card } from "react-bootstrap";
+
 import "./style.css";
 
 function Content({ data, idArticle }) {
   return (
-    <div className="content-app">
-      <Link to={`/article/${idArticle}`}>
-        {/* <ContentTitle title={data.title} /> */}
-        {data.title}
-      </Link>
-      <div className="item">
-        <ContentItem description={data.description} date={data.date} />
-        <ImageItem imageUrl={data.imageUrl} />
-      </div>
-    </div>
+    <Card style={{ width: "60rem", margin: "20px auto" }}>
+      <Card.Body>
+        <div className="content-app">
+          <Link to={`/article/${idArticle}`}>
+            {/* <ContentTitle title={data.title} /> */}
+            {data.title}
+          </Link>
+          <div className="item">
+            <ContentItem description={data.description} date={data.date} />
+            <ImageItem imageUrl={data.imageUrl} />
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
   );
 }
 
