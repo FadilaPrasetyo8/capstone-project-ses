f820d91ae0f2f65e4e01af48a61a5b71a0efc48b
FadilaPrasetyo8 <fadilaprasetyo8@gmail.com>
2022-07-07 20:34:27 +0700

    Update Tampilan Cek Kesehatan

--- src/pages/tentang/Footer.js
+++ src/pages/tentang/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <footer className="blockquote-footer bg-success text-light p-2 text-center mt-2">
+      <footer className="blockquote-footer bg-success text-light p-2 text-center">
         Grup 10 Healty{" "}
         <cite title="Source Title">@Yayasan Anak Bangsa Bisa</cite>
       </footer>
