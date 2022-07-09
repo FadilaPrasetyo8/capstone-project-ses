5e34c269bdb155f08c329e65421d190b0cdc1ee1
FadilaPrasetyo8 <fadilaprasetyo8@gmail.com>
2022-07-09 21:23:54 +0700

    Final Project

--- src/component/edukasi/Edukasi.js
+++ src/component/edukasi/Edukasi.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
 
-const Edukasi = ({title, icons, list, show, onClick}) => {
-    return (
-        <>
-            <div 
-                data-testid="content-item" 
-                className="vis-container" 
-                onClick={onClick}
-            >
-                {title}
-            </div>
-            <img src={icons} alt='icon' className='icons'/>
-            {show ? (
-                    <li data-testid="theList">{list}</li>
-            ): null}
-        </>
-    )
-}
+const Edukasi = ({ title, icons, list, show, onClick }) => {
+  return (
+    <>
+      <div
+        data-testid="content-item"
+        className="vis-container"
+        onClick={onClick}
+      >
+        {title}
+      </div>
+      <img src={icons} alt="icon" className="icons" />
+      {show ? (
+        <li data-testid="theList" className="list">
+          {list}
+        </li>
+      ) : null}
+    </>
+  );
+};
 
-export default Edukasi
+export default Edukasi;
--- src/pages/navbar/Navbar.js
+++ src/pages/navbar/Navbar.js
@@ -1,25 +1,37 @@
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router-dom';
-import React from 'react';
-import './style.css';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
+import React from "react";
+import "./style.css";
 
 const Navigasi = () => {
   return (
-      <Navbar collapseOnSelect expand="lg" bg="success" variant="dark">
-        <Navbar.Brand href="#logo" className='logos'>SES</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-              <Nav.Link as={NavLink} to="/edukasi">Edukasi</Nav.Link>
-              <Nav.Link as={NavLink} to="/cek-kesehatan">Cek Kesehatan</Nav.Link>
-              <Nav.Link as={NavLink} to="/article">Artikel</Nav.Link>
-              <Nav.Link as={NavLink} to="/about" >Tentang Kami</Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    <Navbar collapseOnSelect expand="lg" bg="success" variant="dark">
+      <Navbar.Brand href="#logo" className="logos">
+        SES
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link as={NavLink} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/edukasi">
+            Edukasi
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/cek-kesehatan">
+            Cek Kesehatan
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/article">
+            Artikel
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/about">
+            Tentang Kami
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
-}
+};
 
 export default Navigasi;
